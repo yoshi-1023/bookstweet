ad522763d9a2ae45ebf751f86c38a030de1bdb50
yoshi-1023 <yoshika.122355@gmail.com>
2021-06-25 15:50:00 +0900

    バリデーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :output, presence: true
+  validates :image, presence: true
 end
